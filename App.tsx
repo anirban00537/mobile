2154defe13b7f3f93fea2df8f0578e800680df83
anirban00537 <anirban00537@gmail.com>
2022-06-05 19:58:33 +0600

    screen fixed

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import StackNavigation from './Navigation/StackNavigation';
+import AuthNavigation from './Navigation/AuthNavigation';
 const App = () => {
-  return <StackNavigation />;
+  return <AuthNavigation />;
 };
 
 export default App;
--- Navigation/AuthNavigation.tsx
+++ Navigation/AuthNavigation.tsx
@@ -2,24 +2,25 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigation from './TabNavigation';
-import Details from '../Screens/Details';
+import Login from '../Screens/Login';
+import Signup from '../Screens/Signup';
 
 const Stack = createNativeStackNavigator();
 
-export default function StackNavigation() {
+export default function AuthNavigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="TabNavigation"
-          component={TabNavigation}
+          name="Login"
+          component={Login}
           options={{
             headerShown: false,
           }}
         />
         <Stack.Screen
-          name="Details"
-          component={Details}
+          name="Signup"
+          component={Signup}
           options={{
             headerShown: false,
           }}
