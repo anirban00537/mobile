7a67bd3c75a6b4295fb728977ee5ca3392dd0212
anirban00537 <anirban00537@gmail.com>
2022-06-06 01:10:02 +0600

    login form section done

---
+++ Container/Login/LoginForm.container.tsx
@@ -0,0 +1,71 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import {Button, Input} from 'native-base';
+import {colors} from '../../Constant/colors';
+import Icon from 'react-native-vector-icons/FontAwesome';
+const LoginForm = () => {
+  return (
+    <View style={styles.container}>
+      <Input
+        size="2xl"
+        variant="unstyled"
+        placeholder="Email"
+        style={styles.input}
+      />
+      <Input
+        size="2xl"
+        variant="unstyled"
+        placeholder="Password"
+        type="password"
+        style={styles.input}
+      />
+      <View style={styles.forgetContainer}>
+        <Text style={styles.text}>Forgot Password?</Text>
+        <Icon
+          name="arrow-right"
+          size={20}
+          style={styles.icon}
+          color={colors.gray}
+        />
+      </View>
+      <Button
+        leftIcon={<Icon name="sign-in" size={20} color={colors.white} />}
+        colorScheme="dark"
+        background={colors.primary}
+        marginTop={5}>
+        Login
+      </Button>
+    </View>
+  );
+};
+
+export default LoginForm;
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    paddingHorizontal: 20,
+    marginVertical: 40,
+  },
+  input: {
+    backgroundColor: colors.grayLight,
+    marginTop: 20,
+  },
+  text: {
+    color: colors.black,
+    fontSize: 16,
+    fontWeight: 'bold',
+    // marginTop: 20,
+  },
+  forgetContainer: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  icon: {
+    color: colors.primary,
+    fontSize: 15,
+    marginLeft: 10,
+  },
+});
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,14 +1,31 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {StatusBar, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {colors} from '../Constant/colors';
+import LoginForm from '../Container/Login/LoginForm.container';
 
 const Login = () => {
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <StatusBar barStyle="light-content" backgroundColor={colors.primary} />
+      <Text style={styles.largeText}>Login</Text>
+      <LoginForm />
     </View>
   );
 };
 
 export default Login;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.white,
+    height: '100%',
+  },
+  largeText: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    color: colors.black,
+    marginTop: 20,
+    marginBottom: 20,
+    marginHorizontal: 20,
+  },
+});
